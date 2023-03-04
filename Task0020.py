462f9cdac85ee98668de1b8c7d0bc4dd39681956
KatjaTsch <katjatschebotar.er@mail.ru>
2023-03-04 12:20:21 +0300

    0020

---
+++ Task0020.py
@@ -0,0 +1,35 @@
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое
+# число X в массиве A[1..N]. Пользователь в первой строке вводит
+# натуральное число N – количество элементов в массиве. В последующих
+# строках записаны N целых чисел Ai
+# . Последняя строка содержит число X
+# 5
+# 1 2 3 4 5
+# 3
+# -> 1
+
+A = []
+from random import randint
+N = int(input("Введите число элементов массива: "))
+A=[randint(0, 5) for i in range(N)]
+print(A)
+
+
+X = int(input("Введите искомое число: "))
+count = 0
+for i in range(0, len(A)):
+    if A[i] == X:
+        count += 1
+print(count)
+
+
+# for i in range(N):
+#     A.append(i)
+# print(A)
+
+# A = [int(i) for i in input().split()]
+# print(A)
+# X = int(input())
+# print(A.count(X))
+# for i in range(0, len(N)-1):
+#     print(A[N])
