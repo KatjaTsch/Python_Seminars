09c55b328adc7201402d3470794bb97afe37106a
KatjaTsch <katjatschebotar.er@mail.ru>
2023-03-17 14:50:21 +0300

    Task_0038.py

---
+++ Task_0038.py
@@ -0,0 +1,15 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+A = []
+from random import randint
+n = int(input("Введите размер массива: "))
+min_num = int(input("Введите min: "))
+max_num = int(input("Введите max: "))
+A=[randint(min_num, max_num) for i in range(n)]
+print(A)
+
+for i in range(1, n):
+    if min_num < A[i] < max_num:
+        print(A[i])
