ff7815b43f77f624c7cd7c986d3dd2094e5f1059
KatjaTsch <katjatschebotar.er@mail.ru>
2023-03-25 22:17:28 +0300

    Task_0050Interface.py

--- Task_0050Interface.py
+++ Task_0050Interface.py
@@ -1,6 +1,7 @@
 while True:    
     choise = int(input('Введите нужное действие: \n 1 - Добавить в справочник \
-        \n 2 - Вывести всех \n 3 - Поиск  \n 4 - Выход \n'))
+        \n 2 - Вывести всех \n 3 - Поиск \n 4 - Удалить данные \
+        \n 5 - Изменить данные  \n 6 - Выход \n'))
     match choise:
         case 1:
             import Task_0050file
@@ -11,4 +12,9 @@ while True:
         case 3:
             from Task_0050Search import Search
             Search(input('Введите фамилию: '))
-        case 4: break
+        case 4:
+           import Task_0050del
+        case 5:
+            import Task_Change
+            Task_Change.Change(input ('Введите имя: '), input('Введите номер: ')) 
+        case 6: break
